0d427d6d70690ead21e5e2376fe597bad16d0341
swon95 <shimtkddnjs13@gmail.com>
2023-04-13 18:55:21 +0900

    Web3-React 연동

--- 리액트 + 메타마스크 연동/frontend/src/index.js
+++ 리액트 + 메타마스크 연동/frontend/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Web3ReactProvider } from "@web3-react/core";
+import { getProvider } from "./utills/provider";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Web3ReactProvider getLibrary={getProvider}>
+            <App />
+        </Web3ReactProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
