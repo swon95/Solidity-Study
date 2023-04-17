4847e102240367a2feb0c98a17ed57a3d7c3b4d7
swon95 <shimtkddnjs13@gmail.com>
2023-04-18 01:56:23 +0900

    구매 컨트랙트

--- Animal_Dapp_Project/SaleAnimalToken.sol
+++ Animal_Dapp_Project/SaleAnimalToken.sol
@@ -61,4 +61,64 @@ contract SaleAnimalToken {
 
         onSaleAnimalTokenArray.push(_animalTokenId);
     }
+
+    // 구매 기능 영역
+    function purchaseAnimalToken(uint256 _animalTokenId) public payable {
+        uint256 price = animalTokenPrices[_animalTokenId];
+
+        // 토큰 소유자의 주소 가져오기
+        address animalTokenOwner = mintAnimalTokenAddress.ownerOf(
+            _animalTokenId
+        );
+
+        // require = 조건
+        // price 가 0 보다 커야함.
+        require(price > 0, "Animal token not sale");
+
+        // 함수를 실행할 때 보내는 값의 양이 판매 등록된 가격보다 작거나 같음.
+        require(price <= msg.value, "Caller sent lower than price.");
+
+        // 토큰의 소유자가 아닐경우 true (판매 조건과 반대)
+        require(
+            animalTokenOwner != msg.sender,
+            "Caller is animal token owner."
+        );
+
+        // 함수를 실행한 msg.sender 에 가격 price 만큼의 양 msg.value 이 토큰 주인 animalTokenOwner 에게 전송
+        payable(animalTokenOwner).transfer(msg.value);
+
+        // 인자로 보내는 사람(animalTokenOwner), 받는 사람(msg.sender), 무엇을 보낼것인지(_animalTokenId) 을 넘김
+        mintAnimalTokenAddress.safeTransferFrom(
+            animalTokenOwner,
+            msg.sender,
+            _animalTokenId
+        );
+
+        // 초기화
+        animalTokenPrices[_animalTokenId] = 0;
+
+        // i 는 배열의 길이만큼 반복
+        // 1 개씩 증가
+        for (uint256 i = 0; i < onSaleAnimalTokenArray.length; i++) {
+            // 1 개씩 증가하는 동안
+            // 가격을 초기화 한 animalTokenPrices 에 판매중인 배열을 넣고,
+            // 배열 안에 가격이 0 원인 (_animalTokenId) 를 만나면 제거
+            if (animalTokenPrices[onSaleAnimalTokenArray[i]] == 0) {
+                // 배열의 맨 마지막 인덱스(요소)를 i 와 교체 해주고, 맨 뒤에 위치한 인덱스를 제거
+                onSaleAnimalTokenArray[i] = onSaleAnimalTokenArray[
+                    onSaleAnimalTokenArray.length - 1
+                ];
+                // pop 메소드를 통해 배열의 마지막 요소는 제거되며 기존에 있던 i 는 제거
+                onSaleAnimalTokenArray.pop();
+            }
+        }
+    }
+
+    // front 영역
+    // 판매중인 토큰 배열의 길이를 출력하는 함수
+    // 읽기 전용 view
+    function getSaleAnimalTokenArrayLength() public view returns (uint256) {
+        // 길이를 반환
+        return onSaleAnimalTokenArray.length;
+    }
 }
