7eefe3a13de7fb3fed72df736470bfb87f67e892
swon95 <shimtkddnjs13@gmail.com>
2023-04-23 02:24:05 +0900

    Front(ChakraUI, React-Router-Dom)

--- Animal_Dapp_Project/frontend/src/App.tsx
+++ Animal_Dapp_Project/frontend/src/App.tsx
@@ -1,7 +1,16 @@
 import React, { FC } from "react";
+import { Button } from '@chakra-ui/react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './routes/main';
 
 const App: FC = () => {
-  return <div>web3-boilerplate</div>;
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />}/>
+      </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default App;
